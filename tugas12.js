312a552c71ac6e1b29b9748994a7a0d91d4d76a2
Ramdhawa <shinlia21@gmail.com>
2020-08-09 15:20:12 +0800

    add tugas 12 js

---
+++ tugas12.js
@@ -0,0 +1,10 @@
+function shift_pop()
+{
+  var buah=['Pisang','Jeruk','Apel','Mangga'];
+  console.log(buah);
+  buah.pop();
+  console.log(buah);
+  buah.shift()
+  return buah;
+}
+console.log(shift_pop());
